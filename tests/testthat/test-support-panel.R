a62079867c90fd47c34d78fe55e3bae7d1b679ed
Cam Race <52536248+cjrace@users.noreply.github.com>
2023-11-08 10:56:52 +0000

    Add a couple of extra test case emails

--- tests/testthat/test-support-panel.R
+++ tests/testthat/test-support-panel.R
@@ -1,4 +1,17 @@
 test_that("email needs to follow standard pattern", {
-  expect_error(support_panel(team_email = "thisshoulfail@something"))
+
+  # Testing that it will pass if it follows the something@domain.ending pattern
   expect_no_error(support_panel(team_email = "thisshouldpass@something.com"))
+  expect_no_error(support_panel(team_email = "this.should.pass@something.com"))
+
+
+  # Testing that it will fail if there is no final dot and section
+  expect_error(support_panel(team_email = "thisshoulfail@something"))
+
+  # Test Wales is accepted
+  expect_no_error(support_panel(team_email = "thisshouldpass@something.wales"))
+
+  # Test that it fails if there is no @ symbol
+  expect_error(support_panel(team_email = "team.team"))
+
 })
