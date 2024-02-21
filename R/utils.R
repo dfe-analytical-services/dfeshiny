de6821015aa40fa432842b8456378a79bb076cc7
FOSTER <Charlotte.FOSTER@education.gov.uk>
2024-02-21 15:41:13 +0000

    remove commented-out code

--- R/utils.R
+++ R/utils.R
@@ -98,8 +98,6 @@ custom_disconnect_message <- function(refresh = "Refresh page",
           tags$a(href = "mailto:statistics.development@education.gov.uk", "statistics.development@education.gov.uk"),
           "with details of any problems with this resource."
         )
-        #  ),
-        # htmltools::tags$p("If this persists, you can view tables and data via the ",htmltools::tags$a(href ='https://explore-education-statistics.service.gov.uk/find-statistics/pupil-attendance-in-schools', "Pupil attendance in schools")," release on Explore Education Statistics and please contact statistics.development@education.gov.uk with details of what you were trying to do.")
       )
     ),
     tags$div(id = "ss-overlay", style = "display: none;"),
