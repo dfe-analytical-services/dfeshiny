634c464e07e2b18c1bf57faaa99f7c468aeb56a4
FOSTER <Charlotte.FOSTER@education.gov.uk>
2024-05-13 14:32:05 +0100

    add cookies_panel function

---
+++ R/cookies_panel.R
@@ -0,0 +1,42 @@
+#' Cookies panel
+#'
+#' @description
+#' Create the standard DfE R-Shiny cookies dashboard panel.
+#'
+#' @param cookie_status_output Name of cookie status output object, often
+#' "cookie_status"
+#'
+#' @return a standardised panel for a public R Shiny dashboard in DfE
+#' @export
+#'
+#' @examples
+#' cookies_panel(
+#'   cookie_status_output = "cookie_status"
+#' )
+
+
+
+support_panel <- function(
+    cookie_status_output = "cookie_status") {
+  # Build the support page ----------------------------------------------------
+  shiny::tabPanel(
+    value = "cookies_panel",
+    "Cookies",
+    shinyGovstyle::gov_main_layout(
+      shinyGovstyle::gov_row(
+        shiny::column(
+          width = 12,
+          shiny::tags$h1("Cookies"),
+          shiny::tags$h2("Essential cookies"),
+          shiny::tags$h2("Analytics cookies"),
+          shiny::tags$h2("Use of cookies"),
+          shiny::tags$p("To better understand the reach of our dashboard tools,
+            this site uses cookies to identify numbers of unique users
+            as part of Google Analytics."),
+          shiny::textOutput(cookie_status_output),
+          shiny::actionButton("cookie_consent_clear", "Reset cookie consent"),
+        )
+      )
+    )
+  )
+}
