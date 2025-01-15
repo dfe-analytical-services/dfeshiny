642788cd54d191b69765813aba9e8acb76a52c79
Olu Adetayo <adetayoseun@yahoo.com>
2025-01-15 09:59:30 +0000

    resolve lint issue

--- R/dfe_reactable.R
+++ R/dfe_reactable.R
@@ -1,23 +1,28 @@
 #' Department for Education Reactable Wrapper
 #'
 #' A wrapper around the `reactable` function for creating styled, accessible,
-#' and user-friendly tables tailored to the Department for Education's requirements.
+#' and user-friendly tables tailored to the Department for Education's
+#' requirements.
 #'
 #' @param data A data frame to display in the table.
 #' @param ... Additional arguments passed to `reactable::reactable`.
 #'
 #' @details
-#' The `dfe_reactable` function provides a pre-configured version of the `reactable` function with:
+#' The `dfe_reactable` function provides a pre-configured version of
+#' the `reactable` function with:
 #' \itemize{
 #'   \item **Highlighting**: Row highlighting enabled.
 #'   \item **Borderless Table**: Removes borders for a clean look.
 #'   \item **Sort Icons Hidden**: Sort icons are not displayed by default.
 #'   \item **Resizable Columns**: Users can resize columns.
 #'   \item **Full Width**: Table expands to the full width of the container.
-#'   \item **Default Column Definition**: Custom column header styles, NA value handling,
+#'   \item **Default Column Definition**: Custom column header styles,
+#'   NA value handling,
 #'         and alignment.
-#'   \item **Custom Search Input**: A search bar styled to the Department for Education's specifications.
-#'   \item **Custom Language**: Provides a user-friendly search placeholder text.
+#'   \item **Custom Search Input**: A search bar styled to the Department
+#'   for Education's specifications.
+#'   \item **Custom Language**: Provides a user-friendly search placeholder
+#'   text.
 #' }
 #'
 #' @return A `reactable` HTML widget object.
@@ -33,8 +38,6 @@
 #'   server <- function(input, output, session) {}
 #'   shinyApp(ui, server)
 #' }
-
-
 dfe_reactable <- function(data, ...) {
   reactable::reactable(
     data,
