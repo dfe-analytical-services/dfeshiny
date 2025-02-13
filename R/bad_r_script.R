8d8afe21dbf9ece130e2535d54cb1aa4d013205a
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-02-13 12:32:44 +0000

    try bad commit

---
+++ R/bad_r_script.R
@@ -0,0 +1,8 @@
+bad_function <- function(){print("put everything on one line of function so styler won't like it - make it too long and generally not good practise")}
+
+alsoMakeSomethingInThisFormat <- 'not snake format'
+
+have_some_weird_indents <- data.frame("a" = 1,
+                            "b" = 3,
+                    "c" = 6,
+                                "d" = 0)
