8a09b173d5fa06ddd60e23867e70ec4b133766e7
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-02-13 13:56:53 +0000

    removing bad test script

--- R/bad_r_script.R
+++
@@ -1,9 +0,0 @@
-bad_function <- function(){print("put everything on one line of function so styler won't like it - make it too long and generally not good practise")}
-
-alsoMakeSomethingInThisFormat <- 'not snake format'
-
-have_some_weird_indents <- data.frame("a" = 1,
-                            "b" = 3,
-                    "c" = 6,
-                                "d" = 0)
-# comment
