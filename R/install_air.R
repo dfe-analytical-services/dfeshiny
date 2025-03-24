90b2128d91c6b3284d6b450440aa457b8ad4acee
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-24 12:45:48 +0000

    add auto update argument and file loc checker

--- R/install_air.R
+++ R/install_air.R
@@ -3,6 +3,8 @@
 #' @description checks for air installation status and installs it if
 #' required, updating the global settings if selected
 #'
+#' @param update_global_settings auto update global settings, or don't
+#'
 #' @importFrom data.table like
 #' @export
 #'
@@ -11,7 +13,7 @@
 #' install_air()
 #' }
 
-install_air <- function() {
+install_air <- function(update_global_settings = TRUE) {
   # Check for air and settings - need package data.table to do this
   if ("air" %in% system("ls ~/.config/.", intern = TRUE)) {
     message("installed")
@@ -29,41 +31,57 @@ install_air <- function() {
     }
   }
 
-  configs <- readLines("~/.config/rstudio/rstudio-prefs.json")
-  changes <- 0
+  if (update_global_settings == TRUE) {
+    platform <- Sys.info()[1]
+    if (Sys.getenv("RSTUDIO_CONFIG_DIR") == "") {
+      if (platform == "Windows") {
+        gs_loc <- "~/AppData/Roaming/RStudio/rstudio-prefs.json"
+      } else {
+        gs_loc <- "~/.config/rstudio/rstudio-prefs.json"
+      }
+    } else {
+      gs_loc <- paste0(Sys.getenv("RSTUDIO_CONFIG_DIR"), "/rstudio-prefs.json")
+    }
 
-  if (
-    any(
-      data.table::like(
-        configs,
-        '"code_formatter_external_command": "~/.local/bin/air format"'
-      )
-    )
-  ) {
-    message("referenced")
-  } else {
-    message("not referenced")
-    new_line <- '"code_formatter_external_command": "~/.local/bin/air format",'
-    configs <- append(configs, new_line, after = 1)
-    changes <- changes + 1
-  }
-  if (any(data.table::like(configs, '"reformat_on_save": true'))) {
-    message("active")
-  } else {
-    message("not active: activating now")
-    new_line <- '"reformat_on_save": true,'
-    configs <- append(configs, new_line, after = 1)
-    changes <- changes + 1
-  }
-  if (any(data.table::like(configs, '"code_formatter": "external"'))) {
-    message("external ref")
-  } else {
-    message("no external ref: ref now")
-    new_line <- '"code_formatter": "external",'
-    configs <- append(configs, new_line, after = 1)
-    changes <- changes + 1
-  }
-  if (changes > 0) {
-    writeLines(configs, "~/.config/rstudio/rstudio-prefs.json")
+    if (file.exists(gs_loc)) {
+      configs <- readLines(gs_loc)
+      changes <- 0
+      if (
+        any(
+          data.table::like(
+            configs,
+            '"code_formatter_external_command": "~/.local/bin/air format"'
+          )
+        )
+      ) {
+        message("referenced")
+      } else {
+        message("not referenced")
+        new_line <- '"code_formatter_external_command": "~/.local/bin/air format",'
+        configs <- append(configs, new_line, after = 1)
+        changes <- changes + 1
+      }
+      if (any(data.table::like(configs, '"reformat_on_save": true'))) {
+        message("active")
+      } else {
+        message("not active: activating now")
+        new_line <- '"reformat_on_save": true,'
+        configs <- append(configs, new_line, after = 1)
+        changes <- changes + 1
+      }
+      if (any(data.table::like(configs, '"code_formatter": "external"'))) {
+        message("external ref")
+      } else {
+        message("no external ref: ref now")
+        new_line <- '"code_formatter": "external",'
+        configs <- append(configs, new_line, after = 1)
+        changes <- changes + 1
+      }
+      if (changes > 0) {
+        writeLines(configs, gs_loc)
+      }
+    } else {
+      message(paste0("File: ", gs_loc, " does not exist"))
+    }
   }
 }
