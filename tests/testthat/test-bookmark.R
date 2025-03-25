a442f8185b7b905e522523ebb06b5517077634d9
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-25 13:53:17 +0000

    test loc again

--- tests/testthat/test-bookmark.R
+++ tests/testthat/test-bookmark.R
@@ -1,6 +1,6 @@
 test_that("Bookmark works", {
   # Start local app in the background in test mode
-  bg_app <- shinytest2::AppDriver$new("./tests/test_dashboard/")
+  bg_app <- shinytest2::AppDriver$new("~/tests/test_dashboard")
   # Capture the background app's URL and add appropriate query parameters
   bookmark_url <- paste0(bg_app$get_url(), "?_inputs_&n=10")
   # Open the bookmark URL in a new AppDriver object
