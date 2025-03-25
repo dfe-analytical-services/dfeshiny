7164e282967720f91167ebcd126a33c0df26f39a
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-25 09:58:39 +0000

    check function, further ubuntu/windows split

--- R/install_air.R
+++ R/install_air.R
@@ -4,6 +4,7 @@
 #' required, updating the global settings if selected
 #'
 #' @param update_global_settings auto update global settings, or don't
+#' @param check run to only check whether air is already installed
 #'
 #' @importFrom data.table like
 #' @export
@@ -13,75 +14,86 @@
 #' install_air()
 #' }
 
-install_air <- function(update_global_settings = TRUE) {
-  # Check for air and settings - need package data.table to do this
-  if ("air" %in% system("ls ~/.config/.", intern = TRUE)) {
-    message("installed")
+install_air <- function(update_global_settings = TRUE, check = FALSE) {
+  if (check == TRUE) {
+    if ("air" %in% system("ls ~/.local/bin/.", intern = TRUE)) {
+      message("check complete - air already installed")
+    } else {
+      message("check complete - air is not installed")
+    }
   } else {
-    message("air not installed: installing now")
+    # Check for air and settings - need package data.table to do this
     platform <- Sys.info()[1]
-    if (platform == "Windows") {
-      system(
-        'powershell -ExecutionPolicy Bypass -c "irm https://github.com/posit-dev/air/releases/latest/download/air-installer.ps1 | iex"' # nolint: [object_usage_linter]
-      )
+    if ("air" %in% system("ls ~/.local/bin/.", intern = TRUE)) {
+      message("installed")
     } else {
-      system(
-        "curl -LsSf https://github.com/posit-dev/air/releases/latest/download/air-installer.sh | sh"
-      )
-    }
-  }
-
-  if (update_global_settings == TRUE) {
-    platform <- Sys.info()[1]
-    if (Sys.getenv("RSTUDIO_CONFIG_DIR") == "") {
+      message("air not installed: installing now")
       if (platform == "Windows") {
-        gs_loc <- "~/AppData/Roaming/RStudio/rstudio-prefs.json"
+        system(
+          'powershell -ExecutionPolicy Bypass -c "irm https://github.com/posit-dev/air/releases/latest/download/air-installer.ps1 | iex"' # nolint: [object_usage_linter]
+        )
       } else {
-        gs_loc <- "~/.config/rstudio/rstudio-prefs.json"
+        system(
+          "curl -LsSf https://github.com/posit-dev/air/releases/latest/download/air-installer.sh | sh"
+        )
       }
-    } else {
-      gs_loc <- paste0(Sys.getenv("RSTUDIO_CONFIG_DIR"), "/rstudio-prefs.json")
     }
 
-    if (file.exists(gs_loc)) {
-      configs <- readLines(gs_loc)
-      changes <- 0
-      if (
-        any(
-          data.table::like(
-            configs,
-            '"code_formatter_external_command": "~/.local/bin/air format"'
-          )
+    if (update_global_settings == TRUE) {
+      if (Sys.getenv("RSTUDIO_CONFIG_DIR") == "") {
+        if (platform == "Windows") {
+          gs_loc <- "~/AppData/Roaming/RStudio/rstudio-prefs.json"
+        } else {
+          gs_loc <- "~/.config/rstudio/rstudio-prefs.json"
+        }
+      } else {
+        gs_loc <- paste0(
+          Sys.getenv("RSTUDIO_CONFIG_DIR"),
+          "/rstudio-prefs.json"
         )
-      ) {
-        message("referenced")
+      }
+
+      if (file.exists(gs_loc)) {
+        # R interprets the final line as incomplete - suppress this warning
+        suppressWarnings(configs <- readLines(gs_loc))
+        changes <- 0
+        if (
+          any(
+            data.table::like(
+              configs,
+              '"code_formatter_external_command": "~/.local/bin/air format"'
+            )
+          )
+        ) {
+          message("referenced")
+        } else {
+          message("not referenced")
+          new_line <- '"code_formatter_external_command": "~/.local/bin/air format",'
+          configs <- append(configs, new_line, after = 1)
+          changes <- changes + 1
+        }
+        if (any(data.table::like(configs, '"reformat_on_save": true'))) {
+          message("active")
+        } else {
+          message("not active: activating now")
+          new_line <- '"reformat_on_save": true,'
+          configs <- append(configs, new_line, after = 1)
+          changes <- changes + 1
+        }
+        if (any(data.table::like(configs, '"code_formatter": "external"'))) {
+          message("external ref")
+        } else {
+          message("no external ref: ref now")
+          new_line <- '"code_formatter": "external",'
+          configs <- append(configs, new_line, after = 1)
+          changes <- changes + 1
+        }
+        if (changes > 0) {
+          writeLines(configs, gs_loc)
+        }
       } else {
-        message("not referenced")
-        new_line <- '"code_formatter_external_command": "~/.local/bin/air format",'
-        configs <- append(configs, new_line, after = 1)
-        changes <- changes + 1
+        message(paste0("File: ", gs_loc, " does not exist"))
       }
-      if (any(data.table::like(configs, '"reformat_on_save": true'))) {
-        message("active")
-      } else {
-        message("not active: activating now")
-        new_line <- '"reformat_on_save": true,'
-        configs <- append(configs, new_line, after = 1)
-        changes <- changes + 1
-      }
-      if (any(data.table::like(configs, '"code_formatter": "external"'))) {
-        message("external ref")
-      } else {
-        message("no external ref: ref now")
-        new_line <- '"code_formatter": "external",'
-        configs <- append(configs, new_line, after = 1)
-        changes <- changes + 1
-      }
-      if (changes > 0) {
-        writeLines(configs, gs_loc)
-      }
-    } else {
-      message(paste0("File: ", gs_loc, " does not exist"))
     }
   }
 }
