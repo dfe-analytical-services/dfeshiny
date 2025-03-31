e413635f60ab35cd6641a748577794dbb59ed959
alex-rogers-hub <alex@rogersanalytics.co.uk>
2025-03-31 15:03:00 +0000

    tests!

--- tests/testthat/test-bookmark.R
+++ tests/testthat/test-bookmark.R
@@ -1,11 +1,21 @@
 test_that("Bookmark works", {
-  # Start local app in the background in test mode
   bg_app <- shinytest2::AppDriver$new("../test_dashboard")
   # Capture the background app's URL and add appropriate query parameters
-  bookmark_url <- paste0(bg_app$get_url(), "?_inputs_&n=10")
+  bookmark_url <- paste0(bg_app$get_url(), "?_inputs_&navlistPanel=support_panel")
   # Open the bookmark URL in a new AppDriver object
   app <- shinytest2::AppDriver$new(bookmark_url)
+  # Run your tests on the bookmarked `app`
+  app$expect_values()
 
+  bookmark_url <- paste0(bg_app$get_url(), "?_inputs_&navlistPanel=a11y_panel")
+  # Open the bookmark URL in a new AppDriver object
+  app <- shinytest2::AppDriver$new(bookmark_url)
+  # Run your tests on the bookmarked `app`
+  app$expect_values()
+
+  bookmark_url <- paste0(bg_app$get_url(), "?_inputs_&navlistPanel=cookies_panel_ui")
+  # Open the bookmark URL in a new AppDriver object
+  app <- shinytest2::AppDriver$new(bookmark_url)
   # Run your tests on the bookmarked `app`
   app$expect_values()
 })
