f8be048eb85e5e0e04f8320f1cbf337fea739839
Rich Bielby <richbielby@e.email>
2025-07-07 16:42:31 +0100

    Adding ... as an option in header() in order to allow additional params to shinyGovstyle::header()

--- tests/testthat/test-header.R
+++ tests/testthat/test-header.R
@@ -19,3 +19,14 @@ test_that("outputs are as expected", {
     "dfeshiny/DfE_logo_landscape.png"
   )
 })
+
+test_that("Header handles additional shinyGovstyle header inputs",{
+  expect_equal(
+    header(
+      "Site title",
+      secondary_link = "https://explore-education-statistics.service.gov.uk/"
+      )$children[[2]][[3]][[2]][[3]][[1]]$attribs$href,
+    "https://explore-education-statistics.service.gov.uk/"
+  )
+}
+)
