a6808971a9278d5ed9c6fcc81ca19b35fdb03796
Cam Race <52536248+cjrace@users.noreply.github.com>
2026-02-12 09:05:26 +0000

    update test dashboard tests (thank you CI for reminding me!)

--- tests/test_dashboard/tests/testthat/test-UI-03-external_link.R
+++ tests/test_dashboard/tests/testthat/test-UI-03-external_link.R
@@ -59,26 +59,31 @@ test_that("Link text doesn't have excess whitespace", {
 all_links_html <- app$get_html("a")
 
 test_that("Inspect HTML is as expected", {
-  expect_gt(
-    grep(
-      "target=\"_blank\" rel=\"noopener noreferrer\">R Shiny (opens in new tab)</a>",
-      all_links_html,
-      fixed = TRUE
-    ) |>
-      length(),
-    0
+  visible_link_html <- grep(
+    "target=\"_blank\" rel=\"noopener noreferrer\">R Shiny (opens in new tab)</a>",
+    all_links_html,
+    fixed = TRUE,
+    value = TRUE
   )
+  expect_snapshot(visible_link_html)
 
-  expect_gt(
-    grep(
-      paste0(
-        "<a href=\"https://shiny.posit.co/\" target=\"_blank\" rel=\"noopener no",
-        "referrer\">R Shiny<span class=\"sr-only\"> (opens in new tab)</span></a>"
-      ),
-      all_links_html,
-      fixed = TRUE
-    ) |>
-      length(),
-    0
+  # Remove all whitespace and line breaks for matching as struggling
+  # to get matches otherwise.
+  clean_html <- gsub("\\s+", "", all_links_html)
+  clean_pattern <- gsub(
+    "\\s+",
+    "",
+    paste0(
+      "<a href=\"https://shiny.posit.co/\" class=\"govuk-link\"",
+      "target=\"_blank\" rel=\"noopener noreferrer\">R Shiny<span ",
+      "class=\"sr-only\"> (opens in new tab)</span></a>"
+    )
   )
+  hidden_link_html <- grep(
+    clean_pattern,
+    clean_html,
+    fixed = TRUE,
+    value = TRUE
+  )
+  expect_snapshot(hidden_link_html)
 })
